ffd59de89dd86c9974661cd83b684f9895afece0
Elena María del Río <43640427+Elenadr@users.noreply.github.com>
2021-09-20 12:42:50 +0200

    ejers

---
+++ L1/ejercicio4.asm
@@ -0,0 +1,9 @@
+# Programa que inicializa registro 6 a cero y resta 1
+
+	.text
+	
+	addi x6, x0, 0
+	addi x6, x6, -1
+	
+	li a7, 10
+	ecall
---
+++ L1/ejercicio5.asm
@@ -0,0 +1,11 @@
+# Programa de investigacion
+
+	.text
+	
+	addi x3, x0, 10
+a:
+	addi x3,x3,-1
+	bgt x3,x0, a
+	
+	li a7, 10
+	ecall
