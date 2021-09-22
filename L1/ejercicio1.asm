9d54b9e4005ea3875fa4e7ed3e07ac7b8928cd31
Elena María del Río <43640427+Elenadr@users.noreply.github.com>
2021-09-22 12:28:16 +0200

    Add files via upload

--- L1/ejercicio1.asm
+++ L1/ejercicio1.asm
@@ -3,11 +3,11 @@
 
 	.text 
 	
-	addi x3, x3, 3
-	addi x4, x4, 4
-	addi x5, x5, 5
-	addi x6, x6, 6
-	addi x7, x7, 7
+	addi x3, x0, 3
+	addi x4, x0, 4
+	addi x5, x0, 5
+	addi x6, x0, 6
+	addi x7, x0, 7
 	
 	li a7, 10
 	
