ab25b26a94a1a758115c3f6abc53e7c8872daeaf
Elena Del Rio <elenadelrio@MacBook-Air-de-Elena.local>
2021-09-28 19:53:12 +0200

    Terminados

--- L2/ejercicio10.asm
+++ L2/ejercicio10.asm
@@ -7,15 +7,20 @@
 	addi x7, x0, 3  #-- x7--> c = 7
 	addi x8, x0, 4  #-- x8--> d = 9
 	addi x10, x0, 0	#-- x10--> f = 0
+	addi x11, x0, 15
 	
 bucle:	
 	sub x9, x8, x7   #--  (d-c)
-	addi x9, x9, 15   #--  (d-c + 15)
+	add x12, x9, x11  #--  (d-c) + 15
+	
 	add x11, x5, x6   #--  (a+b)
-	sub x10, x9, x11   #--  (4*x6)-(x7-x8)
-	addi x5, x0, 1   #--  (2*x5)
-	addi x6, x0, 1  #-- x6--> b = 5
-	addi x7, x0, 1  #-- x7--> c = 7
-	addi x8, x0, 1  #-- x8--> d = 9
+	
+	sub x10, x12, x11   #-- (d - c) + 15 - (a + b)
+	
+	#-- Incrementar todos los valores en una unidad
+	addi x5, x5, 1   #--  (2*x5)
+	addi x6, x6, 1  #-- x6--> b = 5
+	addi x7, x7, 1  #-- x7--> c = 7
+	addi x8, x8, 1  #-- x8--> d = 9
 
 b bucle
