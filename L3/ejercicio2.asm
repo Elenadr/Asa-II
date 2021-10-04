afb96b8768a25618acbd131d39fda2782d6af9cd
Elena Del Rio <elenadelrio@MacBook-Air-de-Elena.local>
2021-10-04 13:01:08 +0200

    :pear:

--- L3/ejercicio2.asm
+++ L3/ejercicio2.asm
@@ -30,3 +30,35 @@ e:
 #-- c		0x10010010
 #-- d		0x10010018
 #-- e		0x1001001c
+
+
+#-- 5.    ¿Qué palabra hay almacenada en la dirección 0x10010014?
+#-- Palabra 20
+
+#-- 6.    Rellena esta tabla, indicando qué bytes están almacenados en cada dirección
+#-- Dirección 	Byte
+#-- 0x10010000  01
+
+#-- 0x10010001  00
+
+#-- 0x10010002 00 
+
+#-- 0x10010003 00
+
+#-- 0x10010004 02
+
+#-- 0x10010005 00 
+
+#-- 0x10010006 00
+
+#-- 0x10010007 00
+
+#-- 0x10010018 1e
+
+#-- 0x10010019 00
+
+#-- 0x1001001A
+
+#-- 0x1001001B
+
+#-- 7. -- Vuelca el segmento de datos a un fichero en formato hexadecimal
---
+++ L3/variables.asm
@@ -0,0 +1,19 @@
+#-- Escribe un programa (variables4.s) que tenga un segmento de datos con 4 variables: v1, v2, v3 y v4, 
+#-- inicializadas con los valores 0x12345678, 0x11223344, 0xCACABACA, 0x00FABADA respectivamente. 
+#--En el segmento de código sólo deben estar las instrucciones para terminar
+
+.data 
+
+v1: .word 0x12345678
+
+v2: .word 11223344
+
+v3: .word CACABACA
+
+v4: .word FABADA
+
+
+	.text
+	
+	li a7, 10
+	ecall 
